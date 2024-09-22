c936420252560aab39ea2ed7aab6a01f5c3eba29
Tom Doder <tdoder89@gmail.com>
2024-09-22 14:47:55 +0700

    added media queries for mobile use to several components and changed api url for newly deployed backend

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,14 @@
 import './Header.css'
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+    const navigate = useNavigate()
+
+    const handleHeaderClick = ()  => {
+        navigate('/')
+    }
     return (
-        <div className='header-section'>
+        <div className='header-section' onClick={handleHeaderClick}>
         <h1>Dynasty Fantasy Rankings Checker</h1>
         </div>
     )
